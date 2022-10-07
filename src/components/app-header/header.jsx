92c0805daa2bea0bed9ea6a184be8c94c2c73622
German Vengerov <qasimis@yandex.ru>
2022-10-08 03:46:11 +0400

    fix: scroll

--- src/components/app-header/header.jsx
+++ src/components/app-header/header.jsx
@@ -2,13 +2,13 @@ import "./header.scss";
 
 const Header = () => {
   return (
-        <div className="header">
+        <header className="header">
             <h2 className="header__title">наша продукция</h2>
             <div className="header__cart">
                 <div className="header__goods">3 товара <br /> на сумму 3500 Р</div>
                 <div className="header__logo"></div>
             </div>
-        </div>
+        </header>
   );
 }
 
