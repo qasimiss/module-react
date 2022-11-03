010954d56a2d4660aa14a37d01a70527148b9836
= <qasimis@yandex.ru>
2022-11-03 18:54:42 +0400

    fix uncorrect url

--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -1,34 +1,33 @@
-import "./cart.scss";
-import { useSelector } from "react-redux";
-import CardItem from "../card-item";
-import { useNavigate } from "react-router-dom";
+import "./cart.scss"
+import { useSelector } from "react-redux"
+import CardItem from "../card-item"
+import { useNavigate } from "react-router-dom"
 
 const Cart = () => {
     const navigate = useNavigate()
     const handleAbout = (id) => {
         navigate(`../about/${id}`)
     }
-    const basket = useSelector(state => state.basket.basket)
-    return(
+    const basket = useSelector((state) => state.basket.basket)
+    return (
         <div className="cart">
             <div className="container--min">
-                {basket.map( item => {
-                        return (
-                            <CardItem
+                {basket.map((item) => {
+                    return (
+                        <CardItem
                             circle
-                            id = {item.id}
-                            idx = {item.idx}
-                            url = {item.url}
-                            title = {item.title}
-                            price = {item.price}
-                            handleAbout = {() => handleAbout(item.id)}
-                            />
-                        )
-                    })}
+                            id={item.id}
+                            idx={item.idx}
+                            url={`images/${item.url}`}
+                            title={item.title}
+                            price={item.price}
+                            handleAbout={() => handleAbout(item.id)}
+                        />
+                    )
+                })}
             </div>
-            
         </div>
     )
 }
 
-export default Cart;
+export default Cart
